90235c82884eef56672c6f11217fd38b2d80900d
Paul Lewis <github@aerotwist.com>
2017-03-27 13:14:15 +0100

    Removes unnecessary check
    Fixes #14

--- src/server/middleware/hash-removal.js
+++ src/server/middleware/hash-removal.js
@@ -17,10 +17,6 @@
 
 const hashRemoval = (req, res, next) => {
   const hasHashInName = /[a-f0-9]{64}\./;
-  if (!hasHashInName) {
-    return next();
-  }
-
   req.url = req.url.replace(hasHashInName, '');
   next();
 };
